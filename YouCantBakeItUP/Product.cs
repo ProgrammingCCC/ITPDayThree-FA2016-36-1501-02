82f880d7ff219b021f86aca94e9e1896ec8b97fb
janell <jbaxter@colum.edu>
2016-11-09 14:14:32 -0600

    Code from class

---
+++ YouCantBakeItUP/Product.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+
+namespace YouCantBakeItUP
+{
+    class Product
+    {
+        //attributes
+        public double Price;
+        public string Name;
+        public double Popularity;
+        public string Taste;
+
+        //constructor
+        public Product()
+        {
+
+        }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        //operations
+        public void Decay()
+        {
+            //statements for the method
+        }
+
+        public void About()
+        {
+            Console.WriteLine("The " + Name + " costs $" + Price);
+        }
+    }
+}
---
+++ YouCantBakeItUP/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+/* 
+ * Title of Program 
+ * Your Name, Date
+ * Introduction to Programming
+ */
+
+namespace YouCantBakeItUP
+{
+
+    class Program
+    {
+        static void Main()
+        {
+            Product swissChocolateDonut = new Product("Swiss Chocolate Donut", 2.00);
+            Product BloooooBerryMufen = new Product();
+            BloooooBerryMufen.Name = "BlooooBerry Mufen";
+            BloooooBerryMufen.Price = 3;
+
+            swissChocolateDonut.About();
+            BloooooBerryMufen.About();
+
+            Console.ReadKey();
+        }
+    }
+}
